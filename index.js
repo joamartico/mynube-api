c78ff01f81ab8f5b81b25c0c6e1f7b3522bf1465
Joa Marticorena <joamartico@gmail.com>
2023-02-13 00:12:18 -0300

    PORT = process.env.PORT

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ app.get("/api/getPhoto/", async (req, res) => {
 	res.status(200).json(newPhoto);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
